1543d4d2775d4db5da5ab0b66674600f1ce8bda6
Shafayat621 <syedshafayatayon@gmail.com>
2025-08-03 23:37:24 +0600

    adc

---
+++ braek and continue in python.py
@@ -0,0 +1,11 @@
+for i in range(1,20):
+    if i==11:
+        break
+    print(i)
+
+print("\n")
+
+for j in range(1,20):
+    if j==11:
+        continue
+    print(j)
--- for loop in python.py
+++ for loop in python.py
@@ -1,4 +1,18 @@
-a=1
-while a<=5:
-    print("Hello")
-    a+=1
+#anything itterable can be use for loop
+#using range
+for i in range(10):  #here range(10) means the loop will start from 0 and end in 10
+    print(i)
+print("\n")
+
+for i in range(10,20):  #here range(10,20) means the loop will start from 10 and end in 19
+    print(i)
+print("\n")
+
+for i in range(10,20,4):  #here range(10,20,4) means the loop will start from 10 and end in 19 & the difference will be 4,like 10,14 
+    print(i)
+print("\n")
+
+
+name="syed shafayat ayon"
+for s in name:
+    print (s)
