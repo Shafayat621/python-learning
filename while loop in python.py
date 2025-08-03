ec3c28687689f48cf27a9da2891a87076147cd76
Shafayat621 <syedshafayatayon@gmail.com>
2025-08-03 22:59:57 +0600

    abc

---
+++ while loop in python.py
@@ -0,0 +1,17 @@
+# a=1
+# while a<=5:
+#     print(a)
+#     a+=1
+
+# b=5
+# while b>=1:
+#     print(b)
+#     b-=1
+
+#multiplication table
+# c=int(input("Enter witch number table you want:"))
+# n=1
+# while n<=10:
+#     print(c*n)
+#     n+=1 
+
